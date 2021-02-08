6e749b531d2f538e7b6893f75b2c00582f845c36
hsin-yu <tina2793778@gmail.com>
2021-02-08 22:55:52 +0800

    修改：輸入資料提醒、重置按鈕、全部刪除、單一刪除、BMI 狀態管理

--- js/all.js
+++ js/all.js
@@ -6,19 +6,47 @@ let bmiValue = 0;
 let bmiResult = document.querySelector('.bmi-result');
 let listData = JSON.parse(localStorage.getItem('listData')) || [];
 let bmiResultList = document.querySelector('.bmi-result-list');
-let statusText = '';
 let resultBtnGroup = document.querySelector('.result-btn-group');
+let deleteAllBtn = document.querySelector('.delete-all');
 console.log(btnResult);
 showBmiHistoryData(listData);
 // 點擊後計算 BMI 值
 btnResult.addEventListener('click', calculateBMI);
-resultBtnGroup.querySelector('.recalculate').addEventListener('click', calculateBMI)
-function calculateBMI () {
+// 點擊後回到未輸入資料的畫面
+resultBtnGroup.querySelector('.recalculate').addEventListener('click', BmiResultBtn)
+// 點擊後刪除全部資料
+deleteAllBtn.addEventListener('click',function(){
+  localStorage.removeItem('listData');
+  listData = [];
+  showBmiHistoryData(listData);
+  deleteAllBtn.classList.remove('btn-show');
+})
+// 點擊後刪除特定資料
+bmiResultList.addEventListener('click',function(e){
+  if (e.target.classList.contains('remove-btn')){
+    let resultIndex = Number(e.target.closest('li').dataset.index);
+    console.log(resultIndex);
+    listData.splice(resultIndex,1);
+    localStorage.setItem('listData', JSON.stringify(listData));
+    showBmiHistoryData(listData);
+  }
+})
+function calculateBMI (e) {
+  if (resultBtnGroup.classList.contains('bmi-result-show')) return;
   heightValue = Number(document.querySelector('#height').value);
   weightValue = Number(document.querySelector('#weight').value);
   bmiValue = weightValue / Math.pow(heightValue*0.01,2);
   console.log(heightValue , weightValue);
-  
+  let requiredInputs =  document.querySelectorAll('.required');
+  // console.log(requiredInputs);
+  for (requiredInput of requiredInputs) {
+    // console.log(requiredInput);
+    if (requiredInput.querySelector('input').value === ''){
+      requiredInput.querySelector('.notice').classList.add('show');
+    } else {
+      requiredInput.querySelector('.notice').classList.remove('show');
+    }
+  }
   if (heightValue && weightValue) {
     console.log(heightValue , weightValue);
     console.log(typeof bmiValue);
@@ -26,8 +54,6 @@ function calculateBMI () {
     console.log(bmiStatus(bmiValue));
     addData(heightValue,weightValue,bmiValue);
   }
-  document.querySelector('#height').value = '';
-  document.querySelector('#weight').value = '';
 }
 //加入列表，並同步更新網頁與 localstorage
 function addData(heightValue,weightValue,bmiValue) {
@@ -35,63 +61,51 @@ function addData(heightValue,weightValue,bmiValue) {
   let bmiResult = {
     height: heightValue,
     weight: weightValue,
-    status: bmiStatus(bmiValue),
+    status: bmiStatus(bmiValue).statusText,
     bmi: bmiValue.toFixed(2),
     time: new Date().toLocaleDateString()
   };
   listData.unshift(bmiResult);
   localStorage.setItem('listData', JSON.stringify(listData));
-  showBmiResult(bmiResult.status);
+  showBmiResult(bmiStatus(bmiValue));
   showBmiHistoryData(listData);
 }
 // 判斷 BMI 狀態
 function bmiStatus(bmiValue) {
+  let status = {
+    statusText: '',
+    color: ''
+  };
   if (bmiValue <= 18.5) {
-    statusText = '過輕';
+    status.statusText = '過輕';
+    status.color = '#31BAF9';
   } else if (bmiValue >= 18.5 && bmiValue < 24) {
-    statusText = '理想';
+    status.statusText = '理想';
+    status.color = '#86D73F';
   } else if (bmiValue >= 24 && bmiValue < 27) {
-    statusText = '過重';
+    status.statusText = '過重';
+    status.color = '#FF982D';
   } else if (bmiValue >= 27 && bmiValue < 30) {
-    statusText = '輕度肥胖';
+    status.statusText = '輕度肥胖';
+    status.color = '#FF6C03';
   } else if (bmiValue >= 30 && bmiValue < 35) {
-    statusText = '中度肥胖';
+    status.statusText = '中度肥胖';
+    status.color = '#FF6C03';
   } else {
-    statusText = '重度肥胖';
+    status.statusText = '重度肥胖';
+    status.color = '#FF1200';
   }
-  console.log(statusText);
-  return statusText;
+  console.log(status.statusText);
+  return status;
 }
 
 // 將狀態顯示到畫面上
-function showBmiResult(statusText) {
+function showBmiResult(status) {
   resultBtnGroup.classList.add('bmi-result-show');
   btnResult.innerHTML = `${bmiValue.toFixed(2)}
   <small>BMI</small>`;
-  resultBtnGroup.querySelector('.result-btn-text').textContent = statusText;
-  switch(statusText){
-    case '過輕':
-      changeColor('#31BAF9');
-      break;
-    case '過重':
-      changeColor('#FF982D');
-      break;
-    case '輕度肥胖':
-      changeColor('#FF6C03');
-      break;
-    case '輕度肥胖':
-      changeColor('#FF6C03');
-      break;
-    case '中度肥胖':
-      changeColor('#FF6C03');
-      break;
-    case '重度肥胖':
-      changeColor('#FF1200');
-      break;
-    default:
-      changeColor('#86D73F');
-      break;
-  }
+  resultBtnGroup.querySelector('.result-btn-text').textContent = status.statusText;
+  changeColor(status.color);
 }
 function changeColor(color) {
   btnResult.style['border'] = '6px solid ' + color;
@@ -101,32 +115,17 @@ function changeColor(color) {
 }
 function showBmiHistoryData(listData) {
   bmiResultList.innerHTML = '';
-  for (let i = 0; i <listData.length; i++) {
+  if (listData.length === 0) return;
+  if (listData.length > 1) {
+    deleteAllBtn.classList.add('btn-show');
+  } else {
+    deleteAllBtn.classList.remove('btn-show');
+  }
+  for (let i = 0; i < listData.length; i++) {
     let element = document.createElement('li');
     element.classList.add('bmi-result-item','mb-3');
-    switch(listData[i].status){
-      case '過輕':
-        element.style['border-left'] = '5px solid #31BAF9';
-        break;
-      case '過重':
-        element.style['border-left'] = '5px solid #FF982D';
-        break;
-      case '輕度肥胖':
-        element.style['border-left'] = '5px solid #FF6C03';
-        break;
-      case '輕度肥胖':
-        element.style['border-left'] = '5px solid #FF6C03';
-        break;
-      case '中度肥胖':
-        element.style['border-left'] = '5px solid #FF6C03';
-        break;
-      case '重度肥胖':
-        element.style['border-left'] = '5px solid #FF6C03';
-        break;
-      default:
-        element.style['border-left'] = '5px solid #86D73F';
-        break;
-    }
+    element.setAttribute('data-index', i);
+    element.style['border-left'] = '5px solid' + bmiStatus(listData[i].bmi).color;
     element.innerHTML = `
     <p class="font-size-md">${listData[i].status}</p>
     <ul>
@@ -134,7 +133,24 @@ function showBmiHistoryData(listData) {
     <li class="font-size-md"><span class="font-size-xs mr-1">weight</span> ${listData[i].weight}kg</li>
     <li class="font-size-md"><span class="font-size-xs mr-1">height</span> ${listData[i].height}cm</li>
     </ul>
-    <p class="font-size-xs">${listData[i].time}</p>`;
+    <p class="font-size-xs">${listData[i].time}</p>
+    <button class="remove-btn material-icons">
+      close
+    </button>`;
     bmiResultList.appendChild(element);
     };
+}
+// 回到看結果按鈕
+function BmiResultBtn(e) {
+  e.preventDefault();
+  if (e.target.nodeName === 'button') return;
+  if (e.target.nodeName.toLowerCase() === 'img' || e.target.nodeName.toLowerCase() === 'a') {
+    resultBtnGroup.classList.remove('bmi-result-show');
+    btnResult.innerHTML = `看結果`;
+    resultBtnGroup.querySelector('.result-btn-text').textContent = '';
+    btnResult.style['border'] = 'none';
+    btnResult.style.color = '#424242';
+    document.querySelector('#height').value = '';
+    document.querySelector('#weight').value = '';
+  }
 }
